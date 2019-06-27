921ded3e20e9a3c434d20199fbab1c3740169ba8
Zed Chance <zchance@student.sierracollege.edu>
2019-06-27 14:38:43 -0700

    Player starts at middle of screen

--- engine.py
+++ engine.py
@@ -4,13 +4,16 @@ def main():
    screen_width = 80
    screen_height = 50
    
+   player_x = int(screen_width / 2)
+   player_y = int(screen_height / 2)
+   
    libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
    
    libtcod.console_init_root(screen_width, screen_height, 'RL-ONE', False)
    
    while not libtcod.console_is_window_closed():
       libtcod.console_set_default_foreground(0, libtcod.white)
-      libtcod.console_put_char(0, 1, 1, '@', libtcod.BKGND_NONE)
+      libtcod.console_put_char(0, player_x, player_y, '@', libtcod.BKGND_NONE)
       libtcod.console_flush()
       
       key = libtcod.console_check_for_keypress()
