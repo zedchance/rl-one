9c3e7ce16c79c7c77c67347e5669b511304992e9
Zed Chance <zchance@student.sierracollege.edu>
2019-06-27 17:25:45 -0700

    game_map  now creates procedurally generated maps
    Rect makes rectangular rooms and checks to see if they are overlapping
    
    The engine now has variables for maximum number of rooms, and max and min room size
    
    NPC is still there for demonstration purposes but isn't being placed according to the map so it may be stuck in the wall

--- engine.py
+++ engine.py
@@ -5,32 +5,42 @@ from render_functions import clear_all, render_all
 from map_objects.game_map import GameMap
 
 def main():
+   # Screen size
    screen_width = 80
    screen_height = 50
    
+   # Map and rooms
    map_width = 80
    map_heigth = 45
+   room_max_size = 10
+   room_min_size = 6
+   max_rooms = 30
    
+   # Color dictinoary
    colors = {
       'dark_wall': libtcod.Color(0, 0, 100),
       'dark_ground': libtcod.Color(50, 50, 150)
    }
    
+   # Draw player and other entities
    player = Entity(int(screen_width / 2), int(screen_height / 2), '@', libtcod.white)
-   npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), '@', libtcod.red)
+   npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), 'R', libtcod.red)
    entities = [npc, player]
    
+   # Setup libtcod console
    libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
-   
    libtcod.console_init_root(screen_width, screen_height, 'RL-ONE', False)
-   
    con = libtcod.console_new(screen_width, screen_height)
    
+   # Draw map
    game_map = GameMap(map_width, map_heigth)
+   game_map.make_map(max_rooms, room_min_size, room_max_size, map_width, map_heigth, player)
    
+   # Input controls
    key = libtcod.Key()
    mouse = libtcod.Mouse()
    
+   # Game loop
    while not libtcod.console_is_window_closed():
       libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
       
--- map_objects/game_map.py
+++ map_objects/game_map.py
@@ -1,4 +1,7 @@
+from random import randint
+
 from map_objects.tile import Tile
+from map_objects.rectangle import Rect
 
 class GameMap:
    def __init__(self, width, height):
@@ -7,16 +10,78 @@ class GameMap:
       self.tiles = self.initialize_tiles()
       
    def initialize_tiles(self):
-      tiles = [[Tile(False) for y in range(self.height)] for x in range(self.width)]
-      
-      tiles[30][22].blocked = True
-      tiles[30][22].block_sight = True
-      tiles[31][22].blocked = True
-      tiles[31][22].block_sight = True
-      tiles[32][22].blocked = True
-      tiles[32][22].block_sight = True
-      
+      tiles = [[Tile(True) for y in range(self.height)] for x in range(self.width)]
       return tiles
+      
+   def make_map(self, max_rooms, room_min_size, room_max_size, map_width, map_height, player):
+      # Procedurally generate map
+      rooms = []
+      num_rooms = 0
+      
+      for r in range(max_rooms):
+         # Random width and height
+         w = randint(room_min_size, room_max_size)
+         h = randint(room_min_size, room_max_size)
+         # Random position without going out of bounds of the map
+         x = randint(0, map_width - w - 1)
+         y = randint(0, map_height - h - 1)
+         
+         # "Rect" class makes rectangles easier to work with
+         new_room = Rect(x, y, w, h)
+         
+         # Run through other rooms to see if they intersect
+         for other_room in rooms:
+            if new_room.intersect(other_room):
+               break
+         else:
+            # No intersection, valid room
+            # Paint room to maps tiles
+            self.create_room(new_room)
+            
+            # Center coordinates of new room, to use later
+            (new_x, new_y) = new_room.center()
+            
+            if num_rooms == 0:
+               # First room, where the player should spawn
+               player.x = new_x
+               player.y = new_y
+            else:
+               # All rooms after the first
+               # Connect it to the previous room using a tunnel
+               
+               # Center coordinates of previous room
+               (prev_x, prev_y) = rooms[num_rooms - 1].center()
+               
+               # Flip a coin
+               if randint(0, 1) == 1:
+                  # First move horizontally then vertically
+                  self.create_h_tunnel(prev_x, new_x, prev_y)
+                  self.create_v_tunnel(prev_y, new_y, new_x)
+               else:
+                  # First move vertically then horizontally
+                  self.create_v_tunnel(prev_y, new_y, prev_x)
+                  self.create_h_tunnel(prev_x, new_x, new_y)
+                  
+            # Finally, append new room to the list
+            rooms.append(new_room)
+            num_rooms += 1
+   
+   def create_room(self, room):
+      # Go through the tiles in the rectangle and make them passable
+      for x in range(room.x1 + 1, room.x2):
+         for y in range(room.y1 + 1, room.y2):
+            self.tiles[x][y].blocked = False
+            self.tiles[x][y].block_sight = False
+            
+   def create_h_tunnel(self, x1, x2, y):
+      for x in range(min(x1, x2), max(x1, x2) + 1):
+         self.tiles[x][y].blocked = False
+         self.tiles[x][y].block_sight = False
+         
+   def create_v_tunnel(self, y1, y2, x):
+      for y in range(min(y1, y2), max(y1, y2) + 1):
+         self.tiles[x][y].blocked = False
+         self.tiles[x][y].block_sight = False
 
    def is_blocked(self, x, y):
       if self.tiles[x][y].blocked:
